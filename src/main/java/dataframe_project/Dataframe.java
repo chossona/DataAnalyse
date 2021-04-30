adba0ec7a4666831be85ff1e4aa1f7d7097ee591
Youcef <khelil.youcef1@gmail.com>
2021-04-30 23:28:00 +0200

    Selection une/plusieurs lignes et une/plusieurs colonnes

--- src/main/java/dataframe_project/Dataframe.java
+++ src/main/java/dataframe_project/Dataframe.java
@@ -37,88 +37,128 @@ public class Dataframe {
 	}
 	
 	/**
-	 * Constructeur a partir d'un sous groupe d'un dataframe existant
-	 * @param dataframe Dataframe a partir duquel construire
-	 * @param indexDebut Index de debut
-	 * @param indexFin Index de fin
-	 */
-	public Dataframe(Dataframe dataframe, int indexDebut, int indexFin) {
-	}
-	
-	/**
-	 *  * constructeur a partir d'un fichier csv avec separateur definis a ','
-	 * les labels doivent prealablement etre enleve du csv, il ne doit y avoir que des valeurs
+	 * Constructeur a partir d'un fichier csv.
+	 * Le label est le premier mot, les suivants sont le contenu
 	 * @param filepath
 	 */
 	public Dataframe(String filepath) {
+		parseInit(filepath, ",");
+	}
+	
+	/**
+	 * Constructeur a partir d'un fichier separated value avec separateur donne en argument.
+	 * Le label est le premier mot, les suivants sont le contenu
+	 * @param filepath
+	 * @param separateur
+	 */
+	public Dataframe(String filepath, String separateur) {
+		parseInit(filepath, separateur);
+	}
+	
+	private void parseInit(String filepath, String separator) {
 		this.contenus = new ArrayList<Colonne>();
 		String[] tableau_mot;
 		String ligne ="";
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Boolean creerDataframe = true;
 			while ((ligne = br.readLine()) != null){
-				tableau_mot = ligne.split(","); //chaque champs est separe par une virgule
-				if(creerDataframe) {
-					//si permiere ligne on met en place le dataframe (on creer chaque colonne
-					for(int i = 0; i< tableau_mot.length; i++) {
-						this.contenus.add(new Colonne<String>());
-					}
-					creerDataframe = false;
-				}
-				
-				for(int i = 0; i< tableau_mot.length; i++) {
-					this.contenus.get(i).add(tableau_mot[i]);
+				tableau_mot = ligne.split(separator); //chaque champs est separe par une virgule
+				String label = tableau_mot[0];
+				Colonne<String> colonne = new Colonne<String>();
+				colonne.setLabel(label);
+				this.contenus.add(colonne);
+				//Ajout de chaque mot, on commence a l'indice 1
+				for(int i = 1; i< tableau_mot.length; i++) {
+					colonne.add(tableau_mot[i]);
 				}	
 			} 
 		} catch (FileNotFoundException e) {
 			System.out.println("Fichier inexistant");
-			this.contenus = new ArrayList<Colonne>();
 		} catch (IOException e) {
 			e.printStackTrace();
-			this.contenus = new ArrayList<Colonne>();
-		} 
+		}
 	}
 	
 	/**
-	 * constructeur a partir d'un fichier csv avec separateur donne en argument
-	 * les labels doivent pr�alablement etre enleve du csv, il ne doit y avoir que des valeurs
-	 * @param filepath
-	 * @param separateur
+	 * Creer un dataframe etant un sous groupe du dataframe
+	 * @param labelDebut Label de debut inclus
+	 * @param labelFin Label de fin inclus
+	 * @return
 	 */
-	public Dataframe(String filepath, String separateur) {
-			this.contenus = new ArrayList<Colonne>();
-			String[] tableau_mot;
-			String ligne ="";
-			BufferedReader br;
-			try {
-				br = new BufferedReader(new FileReader(filepath));
-				Boolean creerDataframe = true;
-				while ((ligne = br.readLine()) != null){
-					tableau_mot = ligne.split(separateur); //chaque champs est separe par une virgule
-					if(creerDataframe) {
-						//si permiere ligne on met en place le dataframe (on creer chaque colonne
-						for(int i = 0; i< tableau_mot.length; i++) {
-							this.contenus.add(new Colonne<String>());
-						}
-						creerDataframe = false;
-					}
-					
-					for(int i = 0; i< tableau_mot.length; i++) {
-						this.contenus.get(i).add(tableau_mot[i]);
-					}	
-				} 
-			} catch (FileNotFoundException e) {
-				System.out.println("Fichier inexistant");
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
+	public Dataframe loc(String labelDebut, String labelFin) {
+		Dataframe df = new Dataframe();
+		int i=0;
+		while( i<contenus.size() && contenus.get(i).getLabel() != labelDebut ) i++;
+		while( i<contenus.size() && contenus.get(i).getLabel() != labelFin) {
+			df.addColonne(contenus.get(i));
+			i++;
 		}
+		if( i<contenus.size()) {
+			df.addColonne(contenus.get(i));
+		}
+		return df;
+	}
+	
+
+	/**
+	 * Creer un dataframe etant un sous groupe du dataframe
+	 * @param label Label a selectionner
+	 * @return
+	 */
+	public Dataframe loc(String label) {
+		Dataframe df = new Dataframe();
+		int i=0;
+		while( i<contenus.size() && contenus.get(i).getLabel() != label ) i++;
+		if( i<contenus.size()) {
+			df.addColonne(contenus.get(i));
+		}
+		return df;
+	}
+
+	/**
+	 * Creer un dataframe etant un sous groupe du dataframe
+	 * @param indexDebut Index de debut inclus
+	 * @param indexFin Index de fin inclus
+	 * @return
+	 */
+	public Dataframe iloc(int indexDebut, int indexFin) {
+		Dataframe copyDf = new Dataframe();
+		if(indexDebut>0 && indexFin<this.contenus.size()) {
+			for (int i=0; i<contenus.size(); i++) {
+				Colonne colonne = contenus.get(i);
+				Colonne copyC = new Colonne();
+				copyC.setLabel(colonne.getLabel());
+				for(int j = indexDebut; j<indexFin; j++) {
+					copyC.add(colonne.getElement(j));
+				}
+				copyDf.addColonne(copyC);
+			}
+		}
+		return copyDf;
+	}
 	
 	/**
-	 * aille du dataframe en nombre de colonnes
+	 * Creer un dataframe etant un sous groupe du dataframe
+	 * @param index Index a selectionner
+	 * @return
+	 */
+	public Dataframe iloc(int index) {
+		Dataframe copyDf = new Dataframe();
+		if(index>0 && index<this.contenus.size()) {
+			for (int i=0; i<contenus.size(); i++) {
+				Colonne colonne = contenus.get(i);
+				Colonne copyC = new Colonne();
+				copyC.setLabel(colonne.getLabel());
+				copyC.add(colonne.getElement(index));
+				copyDf.addColonne(copyC);
+			}
+		}
+		return copyDf;
+	}
+	
+	/**
+	 * taille du dataframe en nombre de colonnes
 	 * @return
 	 */
 	public int getSize() {
@@ -134,9 +174,9 @@ public class Dataframe {
 	}
 	
 	public String toString() {
-		String res = "[";
+		String res = "[ ";
 		for (Colonne c : contenus) {
-			res+=c.toString();
+			res+=c.toString() + " ";
 		}
 		return res+"]";
 	}
--- src/test/java/dataframe_project/DataframeTest.java
+++ src/test/java/dataframe_project/DataframeTest.java
@@ -3,6 +3,7 @@ package dataframe_project;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ public class DataframeTest {
 
 	@Test 
 	public void t_ConstrSimple_1() {
-		Colonne c1 = new Colonne<Integer>(1,2,3);
+		Colonne<Integer> c1 = new Colonne<Integer>(1,2,3);
 		Dataframe d = new Dataframe(c1);
 		assertEquals(d.getSize(), 1);//test controlleur1
 	}
@@ -35,8 +36,8 @@ public class DataframeTest {
 	
 	@Test 
 	public void t_ConstrSimple_2() {
-		Colonne c1 = new Colonne<Integer>(1,2,3);
-		Colonne c2  = new Colonne<Integer>(1,5,6,7,8);
+		Colonne<Integer> c1 = new Colonne<Integer>(1,2,3);
+		Colonne<Integer> c2  = new Colonne<Integer>(1,5,6,7,8);
 		Dataframe d = new Dataframe(c1,c2);
 		assertEquals(d.getSize(), 2);//test controlleur 2
 		ArrayList<Colonne> contenu = new ArrayList<Colonne>();
@@ -64,7 +65,7 @@ public class DataframeTest {
 		Colonne<Integer> c2  = new Colonne<Integer>(1,5,6,7,8);
 		Colonne<Integer> c3  = new Colonne<Integer>(1,5,6,7,8);
 		Dataframe d = new Dataframe(c1,c2,c3);
-		assertEquals("[[1,2,3,][1,5,6,7,8,][1,5,6,7,8,]]", d.toString());
+		assertEquals("[ [1,2,3] [1,5,6,7,8] [1,5,6,7,8] ]", d.toString());
 	}
 	
 	@Test 
@@ -73,19 +74,19 @@ public class DataframeTest {
 		Colonne<Integer> c2  = new Colonne<Integer>(1,5,6,7,8);
 		Colonne<String> c3  = new Colonne<String>("papa","a","tromper","maman");
 		Dataframe d = new Dataframe(c1,c2,c3);
-		assertEquals("[[1,2,3,][1,5,6,7,8,][papa,a,tromper,maman,]]", d.toString());
+		assertEquals("[ [1,2,3] [1,5,6,7,8] [papa,a,tromper,maman] ]", d.toString());
 	}
 	
 	@Test
 	public void lecture_csv(){
 		Dataframe d = new Dataframe("exemple_csv.csv");
-		assertEquals("[[12,13,45,46,][benoît,Paul,Pierre,Jacques,]]", d.toString());
+		assertEquals("[ Noms[Pierre,Paul,Jacques] Ages[26,18,58] ]", d.toString());
 	}
 
 	@Test 
 	public void t_lecture_csv_vide() {
 		Dataframe d = new Dataframe("exemple_vide.csv");
-		assertEquals("[]", d.toString());
+		assertEquals("[ ]", d.toString());
 	}
 	
 	@Test 
@@ -95,6 +96,48 @@ public class DataframeTest {
 		}catch (Exception e) {
 			fail("erreur a l'ouverture");
 		}
-		
+	}
+	
+	@Test 
+	public void t_selection_une_colonne() {
+		Colonne<Integer> c1 = new Colonne<Integer>("label1",Arrays.asList(1,2,3));
+		Colonne<Integer> c2 = new Colonne<Integer>("label2",Arrays.asList(4,5,6));
+		Colonne<Integer> c3 = new Colonne<Integer>("label3",Arrays.asList(7,8,9));
+		Dataframe d1 = new Dataframe(c1,c2,c3);
+		Dataframe copyColonne = d1.loc("label2");
+		Dataframe dExpected = new Dataframe(c2);
+		assertEquals(copyColonne.toString(), dExpected.toString());
+	}
+	
+	@Test
+	public void t_selection_plusieurs_colonne() {
+		Colonne<Integer> c1 = new Colonne<Integer>("label1",Arrays.asList(1,2,3));
+		Colonne<Integer> c2 = new Colonne<Integer>("label2",Arrays.asList(4,5,6));
+		Colonne<Integer> c3 = new Colonne<Integer>("label3",Arrays.asList(7,8,9));
+		Colonne<Integer> c4 = new Colonne<Integer>("label4",Arrays.asList(10,11,12));
+		Colonne<Integer> c5 = new Colonne<Integer>("label5",Arrays.asList(13,14,15));
+		Dataframe d = new Dataframe(c1,c2,c3,c4,c5);
+		Dataframe copyColonne = d.loc("label2","label4");
+		Dataframe dExpected = new Dataframe(c2,c3,c4);
+		assertEquals(copyColonne.toString(), dExpected.toString());
+	}
+	
+	@Test
+	public void t_selection_un_ligne() {
+		Colonne<Integer> c1 = new Colonne<Integer>("label1",Arrays.asList(1,2,3));
+		Colonne<Integer> c2 = new Colonne<Integer>("label2",Arrays.asList(4,5,6));
+		Colonne<Integer> c3 = new Colonne<Integer>("label3",Arrays.asList(7,8,9));
+		Dataframe d1 = new Dataframe(c1,c2,c3);
+		Dataframe copyLigne = d1.iloc(1);
+		assertEquals(copyLigne.toString(), "[ label1[2] label2[5] label3[8] ]");
+	}
+	
+	public void t_selection_plusieurs_ligne() {
+		Colonne<Integer> c1 = new Colonne<Integer>("label1",Arrays.asList(1,2,3,4,5));
+		Colonne<Integer> c2 = new Colonne<Integer>("label2",Arrays.asList(6,7,8,9,10));
+		Colonne<Integer> c3 = new Colonne<Integer>("label3",Arrays.asList(11,12,13,14,15));
+		Dataframe d1 = new Dataframe(c1,c2,c3);
+		Dataframe copyLigne = d1.iloc(1,3);
+		assertEquals(copyLigne.toString(), "[ label1[2,3,4] label2[7,8,9] label3[12,13,14] ]");
 	}
 }
